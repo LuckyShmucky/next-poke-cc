64b920edc3b56e90da19744e37034949504445ae
LuckyShmucky <christianluciano567@gmail.com>
2022-10-26 21:24:31 -0700

    updated to next 12. Fix/ the card for each pokemon was initially wrapped in a tag and with the new behavior for links in next that will throw an error. a tag was replaced with a div and issue is resolved

--- components/PokemonItem.js
+++ components/PokemonItem.js
@@ -4,10 +4,10 @@ import pokemonStyles from '../styles/Pokemon.module.css'
 const PokemonItem = ({pokemon}) => {
     return (
         <Link href="/pokemon/[name]" as={`/pokemon/${pokemon.name}`}>
-        <a className={pokemonStyles.card}>
+        <div className={pokemonStyles.card}>
             <h3>{pokemon.name} &rarr;</h3>
             <p>This is the url to find this pokemon: {pokemon.url}</p>
-        </a>
+        </div>
           </Link>   
   )
 }
